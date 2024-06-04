edd6aa662a6bc64fdb7721e5470209dfca1c0ff3
zzo3ozz <wityoung.h@gmail.com>
2024-06-04 20:23:53 +0900

    feat: 채팅 API swagger 작업

--- family-moments/src/main/java/com/spring/familymoments/domain/chat/model/ChatRoomInfo.java
+++ family-moments/src/main/java/com/spring/familymoments/domain/chat/model/ChatRoomInfo.java
@@ -2,6 +2,7 @@ package com.spring.familymoments.domain.chat.model;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.spring.familymoments.domain.user.model.ChatProfile;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -12,15 +13,22 @@ import java.util.List;
 @Getter
 @Builder
 @AllArgsConstructor
+@Schema(description = "채팅방 정보")
 public class ChatRoomInfo {
+    @Schema(description = "가족 식별 정보", example = "1")
     @NotNull
     Long familyId;
+    @Schema(description = "가족명", example = "민니네 가족")
     @NotNull
     String familyName;
+    @Schema(description = "가족 프로필 사진", example = "https://profileImg.url.com")
     String familyProfile;
+    @Schema(description = "채팅방의 마지막 메세지", example = "오늘 저녁 뭐야?")
     String lastMessage;
+    @Schema(description = "채팅방 참여 멤버 정보")
     @JsonInclude(JsonInclude.Include.NON_NULL)
     List<ChatProfile> members;
+    @Schema(description = "읽지 않은 메세지 수, MAX=300", example = "300")
     @JsonInclude(JsonInclude.Include.NON_NULL)
     int unreadMessages;
 }
--- family-moments/src/main/java/com/spring/familymoments/domain/chat/model/MessageRes.java
+++ family-moments/src/main/java/com/spring/familymoments/domain/chat/model/MessageRes.java
@@ -1,6 +1,7 @@
 package com.spring.familymoments.domain.chat.model;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.*;
 
 import javax.validation.constraints.NotNull;
@@ -11,15 +12,20 @@ import java.time.LocalDateTime;
 @Builder
 @Getter
 @Setter
+@Schema(description = "메세지 수신 결과")
 public class MessageRes {
+    @Schema(description = "메세지 식별 번호", example = "660eae160f07a759edbe46a7")
     @NotNull
     private String messageId;
     @JsonInclude(JsonInclude.Include.NON_NULL)
     private Long familyId;
+    @Schema(description = "발신자 id", example = "temp123456")
     @NotNull
     private String sender;
+    @Schema(description = "메세지", example = "오늘 저녁 뭐임?")
     @NotNull
     private String message;
+    @Schema(description = "발신 시각", example = "2024-04-04T22:41:42.457")
     @NotNull
     private LocalDateTime sendedTime;
 }
